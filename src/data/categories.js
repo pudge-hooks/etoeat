92773bc29e01ad3e31e16316fe2604390b2b8f4d
Назар Івченко <nazary9911@gmail.com>
2024-01-23 21:14:58 +0200

    add/data

---
+++ src/data/categories.js
@@ -0,0 +1,64 @@
+const categories = [
+  {
+    id: 'BY1Af0',
+    name: 'Перші страви',
+    img: 'https://1.bp.blogspot.com/-9dAxH0yTxxU/WvSlOS-Au_I/AAAAAAAAAN0/V93LCRg8i18sUTufVZSL6WiXbvDL6SNQACLcBGAs/s1600/Alexey_Lobur_big_287.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af1',
+    name: 'Основні страви',
+    img: 'https://allrecipes.space/wp-content/uploads/2022/07/%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D0%B0-%D1%81%D1%82%D1%80%D0%B0%D0%B2%D0%B0.jpeg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af2',
+    name: 'Закуски',
+    img: 'https://i.pinimg.com/originals/0f/db/92/0fdb9272744c4a1ca6ab0e7ee7dabe9a.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af3',
+    name: 'Сендвічі',
+    img: 'https://www.fonstola.ru/pic/202010/1920x1080/fonstola.ru_411170.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af4',
+    name: 'Салати',
+    img: 'https://cdnn21.img.ria.ru/images/07e7/01/1f/1848737772_0:103:3072:1831_1920x0_80_0_0_67dce23a90906192a4f428a4eca9adb5.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af5',
+    name: 'Гарніри',
+    img: 'https://cdnn21.img.ria.ru/images/152781/74/1527817451_0:105:2000:1230_1920x0_80_0_0_e5e23932752946b442a1ebea74a3550d.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af6',
+    name: 'Десерти',
+    img: 'https://images.wallpaperscraft.ru/image/single/desert_pechene_krem_199539_1920x1080.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af7',
+    name: 'Безалкогольні напої',
+    img: 'https://www.fonstola.ru/pic/201509/1920x1080/fonstola.ru_201811.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af8',
+    name: 'Алкогольні напої',
+    img: 'https://s1.1zoom.me/b5050/224/326269-svetik_1920x1080.jpg',
+    productsList: []
+  },
+  {
+    id: 'BY1Af9',
+    name: 'Коктейлі',
+    img: 'https://s2.best-wallpaper.net/wallpaper/1920x1080/1708/Summer-cocktail-fruit-sea-sunset_1920x1080.jpg',
+    productsList: []
+  }
+]
+
+export default categories;
---
+++ src/data/products.js
@@ -0,0 +1,84 @@
+const products = [
+  {
+    id:'t0vKFs',
+    name: 'Солянка',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'EbbABY',
+    name: 'Бульйон',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'uOntAK',
+    name: 'Чихиртма',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'TFyd1X',
+    name: 'Борщ',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'B5pP5Q',
+    name: 'Суп',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'tGpyx2',
+    name: 'Мівіна',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'ciBLkx',
+    name: 'Гарбузовий Суп',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'W6EVpT',
+    name: 'Вода',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'3R9UeF',
+    name: 'Юшка',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+  {
+    id:'YE09KE',
+    name: 'Уха',
+    compound: 'Мисливські ковбаски, куряче стегно, свинина, шинка, бочок, солений огірок. Подають зі сметаною, лимоном та шоті.',
+    weight: 300,
+    price: 120,
+    img: 'https://daba.rest/wp-content/uploads/2024/01/IMG_0418-scaled-e1706007582734.jpeg'
+  },
+];
+
+export default products;
