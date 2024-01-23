26ae209fe6b217e297c7c6c9c1964e4c297a3505
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2024-01-23 18:49:05 +0200

    project config

---
+++ src/layout/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1>hi from footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/views/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import './Hero.scss';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>hi from hero</h1>
+    </section>
+  );
+};
+
+export default Hero;
