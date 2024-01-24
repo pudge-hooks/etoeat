a43dfc8018de95fa8d326d958d5f094527adace1
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2024-01-24 03:58:13 +0200

    created hero

--- src/views/Hero/Hero.jsx
+++ src/views/Hero/Hero.jsx
@@ -1,11 +1,30 @@
 import './Hero.scss';
+
+import bgVideo from '../../assets/video/promo.mp4';
+import logo from '../../assets/svg/logo.svg';
+import down from '../../assets/svg/arrow-down.svg';
+
 import Container from '../../components/Container/Container';
 
 const Hero = () => {
   return (
     <section className="hero">
-      <Container>
-        <h1 className='fuck-you'>Вирішальная подія 18 року прибила нашого мера гомера до треби</h1>
+      <div className="hero__bg-container">
+        <video
+          src={bgVideo}
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="hero__bg-video"
+        />
+      </div>
+      <div className="hero__bg-overlay"></div>
+      <Container className="hero__container">
+        <img src={logo} alt="логотип" className="hero__logo" />
+        <h1 className="hero__title">Eto Eat</h1>
+        <p className="hero__subtitle">Найкраща доставка їжі у твоєму місті</p>
+        <img src={down} alt="вниз" className="hero__down-icon" />
       </Container>
     </section>
   );
