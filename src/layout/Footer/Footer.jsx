9c677b5ebf41620538eec8a8e2cc505c0e411cd0
Назар Івченко <nazary9911@gmail.com>
2024-01-26 23:11:04 +0200

    feature/footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import './Footer.scss';
+import Container from '../../components/Container/Container';
+import Logo from '../../assets/svg/logo.svg';
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>hi from footer</h1>
+    <footer className='footer'>
+      <Container className='footer__container'>
+        <div>
+          <img className='footer__logo' src={Logo} alt='логотип'/>
+          <h2 className='footer__title'>ETO EAT</h2>
+        </div>
+        <p className='footer__order-number'>Замовити: +380637383933</p>
+        <p className='footer__release'>@2024 release</p>
+      </Container>
     </footer>
   );
 };
