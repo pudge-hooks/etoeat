95cf14088b42378a2058f2c4bb6ca0157be5db83
Назар Івченко <nazary9911@gmail.com>
2024-02-12 22:59:52 +0200

    fix

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,22 +1,8 @@
 import { createPortal } from 'react-dom';
 import preView from '../../assets/img/preview.png';
 import './Preview.scss';
-import sound from '../../assets/sound/sound.mp3';
-import { useEffect, useRef } from 'react';
 
 const Preview = () => {
-
-  const audioRef = useRef(new Audio(sound));
-
-  useEffect(() => {
-    const audio = audioRef.current;
-    audio.play().then(() => {
-      // Аудио воспроизводится успешно
-    }).catch(error => {
-      // Ошибка воспроизведения аудио
-      console.error('Ошибка воспроизведения аудио:', error);
-    });
-  }, []);
 
   return createPortal(
     <div className='preview'>
