bbd24a272cead99ac0810a8415fc628bad8228be
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2024-04-28 19:25:12 +0300

    styled cart

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,28 +2,65 @@ import './Form.scss';
 
 const Form = () => {
   return (
-    <form className='form'>
-      <div className='form__container'>
-        <label>Ім'я</label>
-        <input required />
+    <form className="form">
+      <div class="form__group field">
+        <input
+          type="input"
+          class="form__field"
+          placeholder="Name"
+          required=""
+        />
+        <label for="name" class="form__label">
+          Ім'я
+        </label>
       </div>
-      <div className='form__container'>
-        <label>Прізвище</label>
-        <input required />
+      <div class="form__group field">
+        <input
+          type="input"
+          class="form__field"
+          placeholder="Surname"
+          required=""
+        />
+        <label for="surname" class="form__label">
+          Прізвище
+        </label>
       </div>
-      <div className='form__container'>
-        <label>Номер телефону</label>
-        <input required />
+      <div class="form__group field">
+        <input
+          type="input"
+          class="form__field"
+          placeholder="Number"
+          required=""
+        />
+        <label for="number" class="form__label">
+          Номер телефону
+        </label>
       </div>
-      <div className='form__container'>
-        <label>Адреса</label>
-        <input required />
+      <div class="form__group field">
+        <input
+          type="input"
+          class="form__field"
+          placeholder="Address"
+          required=""
+        />
+        <label for="address" class="form__label">
+          Адреса
+        </label>
       </div>
-      <div className='form__container'>
-        <label>Коментар до замовлення</label>
-        <input />
+      <div class="form__group field">
+        <input
+          type="input"
+          class="form__field"
+          placeholder="Comment"
+          required=""
+        />
+        <label for="comment" class="form__label">
+          Коментар до замовлення
+        </label>
       </div>
-      <button className='form__button' type='submit'>Оформити замовлення</button>
+      <button className="form__button" type="submit">
+        Оформити замовлення
+      </button>
     </form>
   );
 };
