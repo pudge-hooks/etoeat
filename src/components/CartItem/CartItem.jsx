472f6b276fddf119f1ea3e379e29158e13d6dd44
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2024-04-29 15:18:25 +0300

    styled cart

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,9 +1,10 @@
-import { useCart } from "../../data/CartContext";
-import Container from "../Container/Container";
+import { useCart } from '../../data/CartContext';
+import Container from '../Container/Container';
 import Plus from '../../assets/svg/plus.svg';
 import Minus from '../../assets/svg/minus.svg';
 import './CartItem.scss';
-import { useState } from "react";
+import { useState } from 'react';
+import trashIcon from '../../assets/svg/trash.svg';
 
 const CartItem = ({ item, calculateTotalAmount, cartProducts }) => {
   const { removeFromCart } = useCart();
@@ -11,43 +12,52 @@ const CartItem = ({ item, calculateTotalAmount, cartProducts }) => {
   const [count, setCount] = useState(1);
 
   const handlePlus = () => {
-    if(count<99){
+    if (count < 99) {
       item.count = item.count + 1;
       setCount(item.count);
       calculateTotalAmount(cartProducts);
     }
-  }
+  };
 
   const handleMinus = () => {
-    if(count>1){
+    if (count > 1) {
       item.count = item.count - 1;
       setCount(item.count);
       calculateTotalAmount(cartProducts);
     }
-  }
+  };
 
   return (
-    <li className='cart-item'>
-      <Container className='cart-item__container'>
-        <div className='cart-item__photo-container'>
-          <img className='cart-item__photo' src='https://i.postimg.cc/g0m8QJ6t/jpeg-optimizer-DSC-7039.jpg' alt='cart-item' />
+    <li className="cart-item">
+      <Container className="cart-item__container">
+        <div className="cart-item__photo-container">
+          <img
+            className="cart-item__photo"
+            src="https://i.postimg.cc/g0m8QJ6t/jpeg-optimizer-DSC-7039.jpg"
+            alt="cart-item"
+          />
         </div>
-        <div className='cart-item__description'>
+        <div className="cart-item__description">
           <h3>{item.Name}</h3>
-          <div className='cart-item__inner-container'>
-            <div>
+          <div className="cart-item__wrapper">
+            <div className="cart-item__count-wrapper">
               <p>Ціна: {item.Price}грн</p>
-              <div className='cart-item__count-container'>
+              <div className="cart-item__count-container">
                 <button onClick={handlePlus}>
-                  <img className='cart-item__plus' src={Plus} alt='plus'/>
+                  <img className="cart-item__plus" src={Plus} alt="plus" />
                 </button>
                 <p>{count}</p>
                 <button onClick={handleMinus}>
-                  <img className='cart-item__minus' src={Minus} alt='minus'/>
+                  <img className="cart-item__minus" src={Minus} alt="minus" />
                 </button>
               </div>
             </div>
-            <button className='cart-item__remove' onClick={() => removeFromCart(item.ID)}>Видалити</button>
+            <button
+              className="cart-item__remove"
+              onClick={() => removeFromCart(item.ID)}
+            >
+              <img src={trashIcon} alt="delete" />
+            </button>
           </div>
         </div>
       </Container>
